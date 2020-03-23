5abdbfdf60b47c56276d28638eb3a982644c7e9c
ConorDoherty1998 <Conordoherty980@gmail.com>
2020-03-23 18:08:42 +0000

    added bootstrap and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthenticationComponent } from './authentication/authentication/authentication.component';
+import { LoginComponent } from './authentication/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
