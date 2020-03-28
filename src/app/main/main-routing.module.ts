475470333538d38576ea992ef73a5d5547921c03
ConorDoherty1998 <Conordoherty980@gmail.com>
2020-03-28 16:28:43 +0000

    added components and module with routing

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { NewsFeedComponent } from './news-feed/news-feed/news-feed.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component: MainComponent
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: NewsFeedComponent,
+        loadChildren: () => import('./news-feed/news-feed.module').then(m => m.NewsFeedModule)
+      }
+    ]
   }
 ];
 
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,15 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main/main.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ArticleListComponent } from './news-feed/article-list/article-list.component';
+import { ArticleItemComponent } from './news-feed/article-item/article-item.component';
+import { NewsFeedComponent } from './news-feed/news-feed/news-feed.component';
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    ArticleListComponent,
+    ArticleItemComponent,
+    NewsFeedComponent
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule
   ]
 })
 export class MainModule { }
