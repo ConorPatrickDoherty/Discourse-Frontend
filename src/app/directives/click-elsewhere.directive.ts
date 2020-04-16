4edda9a3e39c9651f1b82dbd913e4af75808adc2
ConorDoherty1998 <Conordoherty980@gmail.com>
2020-04-16 16:47:35 +0100

    new directive to sense when the user clicks outside of element

---
+++ src/app/directives/click-elsewhere.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, EventEmitter, ElementRef, HostListener, Output } from '@angular/core';
+
+@Directive({
+  selector: '[appClickElsewhere]'
+})
+export class ClickElsewhereDirective {
+  @Output() clickElsewhere = new EventEmitter<MouseEvent>(); 
+ 
+  constructor(private elementRef: ElementRef) {}
+ 
+  @HostListener('document:click', ['$event'])
+  public onDocumentClick(event: MouseEvent): void {
+    const targetElement = event.target as HTMLElement;
+    if (targetElement && !this.elementRef.nativeElement.contains(targetElement)) {
+        this.clickElsewhere.emit(event);
+    }
+  }
+}
